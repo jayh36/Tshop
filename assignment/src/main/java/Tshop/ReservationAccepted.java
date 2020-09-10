035955052da253474769390e720123c9ceaa167c
jayh36 <jhong0306@naver.com>
2020-09-10 18:39:26 +0900

    first commit

---
+++ assignment/src/main/java/Tshop/ReservationAccepted.java
@@ -0,0 +1,34 @@
+
+package Tshop;
+
+public class ReservationAccepted extends AbstractEvent {
+
+    private Long assignmentId;
+    private Long reservationId;
+    private Long productId;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(Long assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+}
